dd26a6a946f198c0a8abd802f0d3ff1f4e79d22f
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-17 14:50:23 -0300

    Manipulando Strings methodo ataualizando

--- script_9.py
+++ script_9.py
@@ -1,6 +1,11 @@
+
+#Methodo Scriit
 with open("dados.txt", "r", encoding="utf-8") as arquivo:
     print("Representação original da linha")
-
+    contador = 0
     for linha in arquivo:
-        linha_limpa = linha.strip()
-        print(repr(linha_limpa))
+        if linha:
+            contador += 1
+            print("Total  = ", contador)
+       
+       
