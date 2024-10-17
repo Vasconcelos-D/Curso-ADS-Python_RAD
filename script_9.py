08ccc4c553c02b06145ac85c8345be3e1623e599
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-17 15:08:02 -0300

    Manipulando Strings methodo split 2

--- script_9.py
+++ script_9.py
@@ -1,28 +1,11 @@
 # Frase 1: Define uma frase com várias palavras separadas por espaços
 frase1 = "Eu amo comer amoras no café da manhã"
 
-# Converte a frase em uma lista de palavras, separando pelos espaços (padrão do split)
+print("Contagem direta:", frase1.count('amo'))
+contador = 0
+
 lista_termos1 = frase1.split()
-
-# Imprime a lista resultante com as palavras da frase
-print(lista_termos1)
-
-
-# Frase 2: Define uma sequência de palavras com múltiplos espaços entre elas
-frase2 = "Amora  Abacaxi  Abacate  Banana"
-
-# Converte a frase em uma lista de palavras, ignorando os espaços extras
-lista_termos2 = frase2.split()
-
-# Imprime a lista resultante com as frutas separadas
-print(lista_termos2)
-
-
-# Frase 3: Define uma frase com elementos separados por vírgulas
-frase3 = "Carro,Moto,Avião"
-
-# Converte a frase em uma lista, utilizando a vírgula como delimitador
-lista_termos3 = frase3.split(",")
-
-# Imprime a lista resultante com os elementos separados por vírgulas
-print(lista_termos3)
+for termo in lista_termos1:
+    if termo == 'amo':
+        contador += 1
+print("Contagem correta:", contador)
