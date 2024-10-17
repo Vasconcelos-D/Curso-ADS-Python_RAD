c6b9902b2d269d62186c5d01c3e6d96c84a242c9
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-17 09:39:28 -0300

    reabrindo novamente e testando no modo SEEK

--- script6.py
+++ script6.py
@@ -8,3 +8,16 @@ print("Releitura de todo o conteúdo do arquivo")
 print(repr(conteudo_releitura),'\n')
 
 arquivo.close()
+
+arquivo_reaberto = open("dados.txt", "r", encoding='utf-8')
+conteudo_reaberto = arquivo_reaberto.read()
+print("Todo o conteúdo do arquivo novamente")
+print(repr(conteudo_reaberto),'\n')
+
+arquivo_reaberto.seek(8)
+conteudo_seek = arquivo_reaberto.read()
+print("Todo conteúdo do arquivo após o SEEK")
+print(repr(conteudo_seek))
+
+arquivo_reaberto.close()
+# todos em moto de leitura...
