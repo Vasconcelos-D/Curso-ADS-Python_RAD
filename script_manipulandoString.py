0425988471789609617df6e8a47babb2f4cc0c4f
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-17 14:29:39 -0300

    Manipulando Strings readline

--- script_manipulandoString.py
+++ script_manipulandoString.py
@@ -1,7 +1,16 @@
+# Readline
+
 arquivo = open("dados.txt", 'r', encoding='utf-8')
-conteudo = arquivo.read()
+conteudo = arquivo.readline()
 
 print("Tipo de conteúdo:", type(conteudo))
 print("Conteúdo retornado pelo read:")
 print(repr(conteudo))
+
+proximo_conteudo = arquivo.readline()
+print("Próximo conteúdo retornado: ")
+print(repr(proximo_conteudo))
+
+
 arquivo.close()
+
