1a4a79edc1598c568f616f4b78189bf01e49d086
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-17 09:50:46 -0300

    Reabrindo com FOR para ler os arquivos dentro

--- script2.py
+++ script2.py
@@ -1,9 +1,11 @@
-arquivo = open("dados.txt")
+arquivo = open("dados.txt", "r", encoding='utf-8')
 
-print("Nome do arquivo:", arquivo.name)
-print("Modo do arquivo:", arquivo.mode)
-print("Arquivo fechado:", arquivo.closed)
+print("Iterando sobre o arquivo")
 
+for linha in arquivo:
+    print(repr(linha))
+    
 arquivo.close()
 
-print("Arquivo fechado?", arquivo.closed)
+#usando for para pecorrer todas as linhas e imprimir na tela, usando para arquivos com muitas 
+#linha de codigos. 
