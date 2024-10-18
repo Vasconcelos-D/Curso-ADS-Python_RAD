9ada7847bcc9825a002a516900bf4cb48bd2c714
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-18 09:55:28 -0300

    CODIFICANDO PALAVRAS EM PY

--- script_string.py
+++ script_string.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+'''from datetime import datetime
 
 nome = "Alucard"
 idade = 25
@@ -58,3 +58,31 @@ minha_data_formatada = f'A data de hoje formatada é {data:%d/%m/%y}'
 
 print(minha_data)
 print(minha_data_formatada)
+'''
+def zenit_polar_replace(text):
+    # Aplicar a codificação ZENIT POLAR utilizando o método replace
+    replacements = [('z', 'p'), ('e', 'o'), ('n', 'l'), ('i', 'a'), ('t', 'r'),
+                    ('Z', 'P'), ('E', 'O'), ('N', 'L'), ('I', 'A'), ('T', 'R')]
+    for old, new in replacements:
+        text = text.replace(old, new)
+    return text
+ 
+def main():
+    # Entrada da frase e aplicação da codificação
+    phrase = "The quick brown fox jumps over the lazy dog"
+    phrase = phrase.title()  # Primeira letra de cada palavra em maiúscula
+ 
+    # Dividir a frase em palavras
+    words = phrase.split()
+ 
+    # Processar cada palavra na lista usando ZENIT POLAR
+    coded_words = [zenit_polar_replace(word) for word in words]
+ 
+    # Juntar todas as palavras codificadas em uma frase
+    coded_phrase = " ".join(coded_words)
+    print("Original:", phrase)
+    print("Coded:", coded_phrase)
+ 
+if __name__ == "__main__":
+    main()
+# CODIFICANDO PALAVRAS EM PYTHON
