3ae7cdaeaaf8578d41534c799c2109337742be87
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-31 16:58:15 -0300

    Atualiando

--- interface grafica/script_interface_grafica.py
+++ interface grafica/script_interface_grafica.py
@@ -40,9 +40,30 @@
 # if __name__ == '__main__':
 #     main()
 
-from kivy.app import App
-from kivy.uix.button import Button
+# from kivy.app import App
+# from kivy.uix.button import Button
 
-class ExemploApp(App):
-    def build(self):
-        return Button(text='Olá, Mundo!')
+# class ExemploApp(App):
+#     def build(self):
+#         return Button(text='Olá, Mundo!')
+
+import pyforms
+from pyforms.basewidget import BaseWidget
+from pyforms.controls import ControlText
+from pyforms.controls import ControlButton
+
+class ExemploSimples(BaseWidget):
+
+    def __init__(self):
+        super(ExemploSimples,self).__init__('ExemploSimples')
+        #Definition of the forms fields
+        self._nome = ControlText('Nome', 'Default value')
+        self._sobrename = ControlText('Sobrenome')
+        self._nomeCompleto = ControlText('Nome completo')
+        self._button = ControlButton('Pressione o Botão')
+
+
+#Execute the application
+if __name__ == "__main__":
+    from pyforms import start_app
+    start_app(ExemploSimples)
