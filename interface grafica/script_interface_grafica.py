535b0812a5b827a74e7c3533b7c5a8861fefd13e
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-10-31 15:26:20 -0300

    Teste com o Framework Gui Flex

--- interface grafica/script_interface_grafica.py
+++ interface grafica/script_interface_grafica.py
@@ -4,3 +4,13 @@
 #tkinter._test()
 #Sucess
 
+from flexx import flx
+class Exemplo(flx.Widget):
+    def init(self):
+        flx.Button(text='Olá')
+        flx.Button(text='Mundo')
+
+if __name__ == '__main__':
+    a = flx.App(Exemplo, title = 'Flexx demostração')
+    m = a.launch()
+    flx.run()
